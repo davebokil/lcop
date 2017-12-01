65c4b201dc4f36eddd403297408ea1a9611876ad
Dave Bokil <bokild@gmail.com>
2017-12-01 10:50:04 -0500

    mikes email and line break

--- js/main.js
+++ js/main.js
@@ -286,6 +286,6 @@
 
 
 var contactform = document.getElementById('contact');
-contactform.setAttribute('action', '//formspree.io/' + 'bokild' + '@' + 'gmail' + '.' + 'com');
+contactform.setAttribute('action', '//formspree.io/' + 'mtr72' + '@' + 'docs' + '.' + 'rutgers' + '.' + 'edu');
 
 }());
